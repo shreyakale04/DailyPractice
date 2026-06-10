d4826d993f0cf211a530a97853a57ef4d97f095f
shreyakale08 <kaleshreya347@gmail.com>
2026-06-10 14:11:21 +0530

    Stack Example

---
+++ src/com/StackEX/StackPop.java
@@ -0,0 +1,60 @@
+package com.StackEX;
+import java.util.*;
+
+public class StackPop {
+
+	public static void main(String[] args) {
+		
+		//creating an object of Stack class  
+		Stack <Integer> stk = new Stack<>();
+		System.out.println("stack: " + stk);
+		
+		//pushing elements into the stack  
+		pushelmnt(stk, 10);
+		pushelmnt(stk, 20);
+		pushelmnt(stk, 13);
+		pushelmnt(stk, 15);
+		pushelmnt(stk, 18);
+		pushelmnt(stk, 19);
+		
+		//popping elements from the stack 
+		
+		popelmnt(stk);
+		popelmnt(stk);
+		popelmnt(stk);
+		
+		//throws exception if the stack is empty  
+		
+		try{
+			popelmnt(stk);	
+		}
+		catch(EmptyStackException e){
+			System.out.println("empty stack");
+		}
+		
+	}
+	//performing push operation  
+
+	static void pushelmnt(Stack stk, int x){
+		
+		//invoking push() method 
+		stk.push(new Integer(x));
+		System.out.println("push -> " + x); 
+		
+		//prints modified stack  
+		System.out.println("stack: " + stk);  
+	}
+	//performing pop operation  
+	static void popelmnt(Stack stk)   
+	{  
+	System.out.print("pop -> ");
+	
+	//invoking pop() method   
+	Integer x = (Integer) stk.pop();  
+	System.out.println(x);  
+	
+	//prints modified stack  
+	System.out.println("stack: " + stk);  
+	
+	}
+}
