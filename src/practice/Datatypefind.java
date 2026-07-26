6d73ce6c539cf27ad5bb439a26a795d08812f8ec
shreyakale08 <kaleshreya347@gmail.com>
2026-07-26 23:02:38 +0530

    Find Datatype

---
+++ src/practice/Datatypefind.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Datatypefind {
+
+	public static void main(String[] args) {
+	 String a = "name";
+	 
+	 System.out.println(((String)a).getClass().getSimpleName());
+
+	}
+
+}
