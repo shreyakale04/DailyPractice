e6832590b83362b5ae594d906ed0c1fdcd323ea4
shreyakale08 <kaleshreya347@gmail.com>
2026-08-19 22:36:53 +0530

    Run Length Encoding Problem

---
+++ src/practice/RunLengthEncoding.java
@@ -0,0 +1,27 @@
+package practice;
+
+public class RunLengthEncoding {
+
+	public static void main(String[] args) {
+		String str = "ddeeggg";
+		
+		StringBuilder sb = new StringBuilder();
+		int count = 1;
+		
+		for(int i = 1; i<str.length(); i++){
+			
+			if(str.charAt(i) == str.charAt(i-1)){
+				
+				count++;
+			}
+			else{
+			    sb.append(str.charAt(i-1)).append(count);
+			    count = 1;
+			}
+		}
+		sb.append(str.charAt(str.length()-1)).append(count);
+          System.out.println(sb);		
+
+	}
+
+}
